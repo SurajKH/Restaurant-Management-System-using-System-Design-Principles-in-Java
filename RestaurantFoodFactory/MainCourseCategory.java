d2edeb08561ca1c297cb6ba1a6b2d9e052d30796
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-06-05 20:51:55 +0530

    Add files via upload

---
+++ RestaurantFoodFactory/MainCourseCategory.java
@@ -0,0 +1,44 @@
+package RestaurantFoodFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainCourseCategory extends Food {
+	
+//	String mealID;
+//	String mealName;
+//	String OrderTimeStamp;
+//	String mealCategory;
+//	String mealType;
+
+	public MainCourseCategory(String mealID,String mealName,String mealCategory,String mealType,int orderTotal)
+	{
+		this.mealID=mealID;
+		this.mealName=mealName;
+		this.mealCategory=mealCategory;
+		this.mealType=mealType;
+		this.orderTotal=orderTotal;
+	}
+	
+	@Override
+	public void OrderDetails() {
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+	    Date date = new Date();  
+	    
+		System.out.println("Order Summary: ");
+		System.out.println();
+        System.out.println("Current TimeStamp:\n"+formatter.format(date));
+		System.out.println();
+		
+		System.out.println("Order Summary: ");
+		System.out.println("MAIN COURSE: ");
+		System.out.println("MEAL ID: "+this.mealID);
+		System.out.println("MEAL NAME: "+this.mealName);
+		System.out.println("MEAL CATEGORY: "+this.mealCategory);
+		System.out.println("MEAL TYPE: "+this.mealType);
+		System.out.println("ORDER TOTAL: "+this.orderTotal);
+
+		
+	}
+}
